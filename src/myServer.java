3262b7d29c1418b9b71f65cd77b0474fed23b4ac
Alexnorvag <norvag@mail.ru>
2017-08-08 14:47:17 +0300

    multi client

--- src/myServer.java
+++ src/myServer.java
@@ -8,30 +8,16 @@ public class myServer  {
 
     public static void main(String[] args) throws IOException {
         ServerSocket s = new ServerSocket(PORT);
-        System.out.println("Started: " + s);
+        System.out.println("Server Started");
         try {
-            //blocks until there is a connection
-            Socket socket = s.accept();
-            try {
-                System.out.println("Connection accepted: " + socket);
-                BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-                PrintWriter out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socket.getOutputStream())), true);
-                while (true) {
-                    String str = in.readLine();
-                    if (str.equals("END"))
-                        break;
-                    System.out.println("Echoing: " + str);
-                    out.println(str);
-                }
+            while (true) {
+                //blocks until there is a connection
+                Socket socket = s.accept();
+                new ServerOne(socket);
             }
-            finally {
-                System.out.println("closing...");
-                socket.close();
-            }
-        }
-        finally {
+        } finally {
             s.close();
         }
-    }
 
+    }
 }
